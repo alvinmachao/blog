1a23f3376eec8765c26fe80f3f3d6aa4aea767d7
alvinmachao <821603741@qq.com>
2016-10-02 15:48:49 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,32 +1,8 @@
 /**
- * Created by Alvin on 2016/9/20.
+ * Created by Alvin on 2016/10/1.
  */
-var indexRender = (function () {
 
-    var content = document.getElementsByClassName('content')[0];
-    var problem = document.getElementsByClassName('problem')[0];
-    var ok = document.querySelector('.ok');
-    var cancel = document.querySelector('.cancel');
-    var p3Click = document.querySelector('#p3Click');
-
-    var p8Content = '一个满怀梦想，满怀希望，打拼的在前端岗位上的我，依然保持着最初的激情，我一直认为无论是生活还是工作，都应该以最饱满的热情去迎接，生活中的我比较爱说话，喜欢交朋友，也喜欢运动，我本身就是个急性子的人，在工作中体现的就更加明显，有任务的话不喜欢拖到最后，尽可能的早些完成，留出足够的时间去做最终的完善。',
-        p3Content = '如果您遇到了以上的这些情况,如果您正在寻找一名全栈工程师,那么巧了,同样我也正在寻求一份稳定长知识的工作.';
-    p8Content = p8Content.split('');
-    p3Content = p3Content.split('');
-    //js打字
-    function type(curE, arr, num) {
-        var n = 0;
-        var str1 = '';
-        clearInterval(curE.temp);
-        curE.temp = setInterval(function () {
-            str1 += arr[n];
-            curE.innerHTML = str1;
-            n++;
-            if (n == arr.length) {
-                clearTimeout(curE.temp);
-            }
-        }, num);
-    }
+var personalRender = (function () {
 
 
     /*swiper*/
@@ -49,12 +25,6 @@ var indexRender = (function () {
                             break;
                         default :
                             item.classList.remove('p' + index + 'Animate');
-                            if (index == 8) {
-                                content.innerHTML = "";
-                            }
-                            if (curIndex == 3) {
-                                problem.innerHTML = '';
-                            }
                             break;
                     }
                     if (curIndex == index) {
@@ -67,57 +37,43 @@ var indexRender = (function () {
                                 break;
                             default :
                                 item.classList.add('p' + curIndex + 'Animate');
-
-                                if (curIndex == 8) {
-                                    clearInterval(window.timer1);
-                                    window.timer1 = setTimeout(type(content, p8Content, 200), 1500);
-                                }
-                                if (curIndex == 3) {
-                                    type(problem, p3Content, 200);
-                                }
                                 break;
                         }
                     }
                 });
             }
         });
-        ok.addEventListener('touchend', function () {
-            mySwiper.slideNext();
-        });
-        cancel.addEventListener('touchend', function () {
-            var a = mySwiper.activeIndex;
-            var slides = mySwiper.slides;
-            slides[a].classList.remove('p' + a + 'Animate');
-            clearTimeout(cancel.time);
-            cancel.time = setTimeout(function () {
-                slides[a].classList.add('p' + a + 'Animate')
-            }, 200);
-            //mySwiper.slideTo(a);
-        });
-        p3Click.addEventListener('touchend', function () {
-            mySwiper.slideNext();
-        });
     }
 
     return {
         init: function () {
-            //初始化宽度
-            !function () {
-                var desW = 320,
-                    winW = document.documentElement.clientWidth,
-                    oMain = document.querySelector('.main');
-                if (winW > 640) {
-                    oMain.style.width = 640 + 'px';
-                    document.documentElement.style.fontSize = '200px';
-                    return;
-                }
-                document.documentElement.style.fontSize = winW / desW * 100 + 'px';
-            }();
-           
-            //执行swiper
-            sw();
+            //music 处理：
+            (function () {
+                var oMusicBox = document.getElementById('music');
+                var oAudio = oMusicBox.getElementsByTagName('audio')[0];
+                window.setTimeout(function () {
+                    oAudio.volume = .5;
+                    oAudio.play();
+                    oAudio.addEventListener('canplay', function () {
+                        oMusicBox.style.display = 'block';
+                        oMusicBox.className = 'music move';
+                    }, false);
 
+                }, 1000);
+                oMusicBox.addEventListener('click', function () {
+                    if (oAudio.paused) {
+                        oAudio.play();
+                        oMusicBox.className = 'music move';
+                        return;
+                    }
+                    oAudio.pause();
+                    oMusicBox.className = 'music';
+                }, false)
+            })();
+            //swiper
+            sw();
         }
     }
 })();
-indexRender.init();
+
+personalRender.init();
